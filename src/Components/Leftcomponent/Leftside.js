53d0aa1b20023a65680301fd7594fc6b05438cc5
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-05-03 09:43:51 +0100

    added icons

--- src/Components/Leftcomponent/Leftside.js
+++ src/Components/Leftcomponent/Leftside.js
@@ -2,6 +2,8 @@ import "./Leftside.css";
 import React from "react";
 import img from "../../Img/20294366_959186364224254_897572492560946227_n.jpg";
 import { RxGithubLogo } from "react-icons/rx";
+import { AiFillTwitterCircle } from "react-icons/ai";
+import { SiLinkedin } from "react-icons/si";
 
 const Leftside = () => {
   return (
@@ -12,6 +14,16 @@ const Leftside = () => {
         <a href="https://github.com/ceasar28">
           <i className="Icon">
             <RxGithubLogo />
+          </i>
+        </a>
+        <a href="https://github.com/ceasar28">
+          <i className="Icon">
+            <AiFillTwitterCircle />
+          </i>
+        </a>
+        <a href="https://github.com/ceasar28">
+          <i className="Icon">
+            <SiLinkedin />
           </i>
         </a>
       </div>
