014212e25c50b81abe23e8d3773ce0c6abfff565
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-05-03 08:10:28 +0100

    left profile pics

--- src/Components/Leftcomponent/Leftside.js
+++ src/Components/Leftcomponent/Leftside.js
@@ -1,8 +1,13 @@
 import "./Leftside.css";
 import React from "react";
+import img from "../../Img/20294366_959186364224254_897572492560946227_n.jpg";
 
 const Leftside = () => {
-  return <div> men</div>;
+  return (
+    <div>
+      <img src="../img" />
+    </div>
+  );
 };
 
 export default Leftside;
