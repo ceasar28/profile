55b1e696d85db454a9a1007c9961f2083962e08e
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-05-03 14:39:05 +0100

    Right side

--- src/Components/Leftcomponent/Leftside.js
+++ src/Components/Leftcomponent/Leftside.js
@@ -1,6 +1,7 @@
 import "./Leftside.css";
 import React from "react";
-import img from "../../Img/20294366_959186364224254_897572492560946227_n.jpg";
+// import img from "../../Img/20294366_959186364224254_897572492560946227_n.jpg";
+import profilePics from "../../Img/profile image.jfif";
 import { RxGithubLogo } from "react-icons/rx";
 import { AiFillTwitterCircle } from "react-icons/ai";
 import { SiLinkedin } from "react-icons/si";
@@ -8,7 +9,7 @@ import { SiLinkedin } from "react-icons/si";
 const Leftside = () => {
   return (
     <div className="left">
-      <img src={img} alt="" />
+      <img src={profilePics} alt="" />
       <h2>Emmanuel Ekete</h2>
       <div className="Socials">
         <a href="https://github.com/ceasar28">
--- src/Components/Rightcomponents/Overview.js
+++ src/Components/Rightcomponents/Overview.js
@@ -4,10 +4,40 @@ const Overview = () => {
     <div className="overview">
       <h2>Hello</h2>
       <p>
-        I am passionate Software Developer with keen interest in the technology
-        driven world of possibilities. "As a first principles engineer. I want
-        to know how things work under the hood. Look at Nature and see endless
-        possibilities"
+        I am a software developer at Foxy Labs Technology Limited, with a keen
+        interest in the technology-driven world of possibilities. I am effective
+        at learning and working on tech-related projects, which has helped me
+        gain experience in various fields. I have worked as an Embedded Software
+        Engineering Intern at the Scientific Equipment Development Institute
+        (SEDI) and Buchi West Agro Allied Services, where I wrote codes for
+        arduino embedded systems and raspberry pi for automating food processing
+        machines, developed a software for relay switch control of a grinding
+        machine, and developed an IoT monitoring software for a tomatoes
+        Hydroponics system.
+      </p>
+
+      <p>
+        I hold a Bachelor of Engineering in Mechanical and Production
+        Engineering from Enugu State University of Science and Technology, and a
+        West African Senior School Certificate in Science from Federal
+        Government College Enugu. I have skills in Shell, Javascript, HTML, CSS,
+        Python, C++, Matlab Analysis, SQL, NoSQL (Mongodb), leadership,
+        teamwork, and learning.
+      </p>
+
+      <p>
+        I have also been involved in various volunteer works such as developing
+        an attendance management Web app using node.js, express.js, EJS
+        (template) and Mongodb as part of National Youth Service Corps Community
+        Development service, designing and programming a hydroponics tomato
+        watering and security system (IOT) using C++ and arduino as part of
+        ENACTUS student competition, and collaborating with a team of four to
+        develop a result computational software for school result computations
+        as part of ENACTUS student competition.
+      </p>
+      <p>
+        My interests include software development, robotic and automation,
+        machine learning, and blockchain technology.
       </p>
     </div>
   );
