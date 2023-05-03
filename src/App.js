9b65a1c209b64c0a10e16956cd5b2a0fa4c82691
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-05-03 09:16:41 +0100

    added icons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Leftside from "./Components/Leftcomponent/Leftside";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Leftside />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Leftcomponent/Leftside.js
+++ src/Components/Leftcomponent/Leftside.js
@@ -1,11 +1,20 @@
 import "./Leftside.css";
 import React from "react";
 import img from "../../Img/20294366_959186364224254_897572492560946227_n.jpg";
+import { RxGithubLogo } from "react-icons/rx";
 
 const Leftside = () => {
   return (
-    <div>
+    <div className="left">
       <img src={img} alt="" />
+      <h3>Emmanuel Ekete</h3>
+      <div className="Socials">
+        <a href="https://github.com/ceasar28">
+          <i className="Icon">
+            <RxGithubLogo />
+          </i>
+        </a>
+      </div>
     </div>
   );
 };
