543ec775016892d8b3283473a4bbe142017ff918
Ekete Emmanuel <emmanuelugochukwuekete@gmail.com>
2023-05-04 13:25:56 +0100

    sending http request

--- src/Components/Rightcomponents/Rightnav.js
+++ src/Components/Rightcomponents/Rightnav.js
@@ -1,14 +1,31 @@
 import "./Rightnav.css";
+import { useState } from "react";
 
 const Rightnav = () => {
+  const [movies, setMovies] = useState([]);
   function FetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films/");
+    fetch("https://swapi.dev/api/films/")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const transformedMovies = data.results.map((moviesData) => {
+          return {
+            id: moviesData.episode_id,
+            title: moviesData.title,
+            openingText: moviesData.opening_crawl,
+            releaseDate: moviesData.release_date,
+          };
+        });
+        console.log(transformedMovies);
+        setMovies(transformedMovies);
+      });
   }
   return (
     <div>
       <ul className="nav">
         <li>Overview</li>
-        <li>Repository</li>
+        <li onClick={FetchMoviesHandler}>Repository</li>
       </ul>
     </div>
   );
